6d19e4fb463a5c799b3d8868358e31d8145e7573
kaustavgupta <kaustavgupta2311@gmail.com>
2025-08-17 13:27:37 +0530

    first commit

---
+++ react.jsx
@@ -0,0 +1,10 @@
+//! what was the need or react?
+/*
+# 1. state was not being easily managed.
+# 2. ghost message problem
+# 3. DOM aapas mein dhang se baat nai kr rhi thi
+# 4. async dhang se nai chalta tha
+*/
+
+// how should 
+
