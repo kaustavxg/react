1c2cc1e57796ac1169d29c3d231df2665b109351
kaustavgupta <kaustavgupta2311@gmail.com>
2025-09-21 23:41:48 +0530

    counter app

--- use-state-hook/src/App.jsx
+++ use-state-hook/src/App.jsx
@@ -6,17 +6,25 @@ function App() {
   const [count, setCount] = useState(0);
 
   function addValue(){
-   setCount(prev => prev + 1);
+  setCount(prevCounter => prevCounter + 1); // 0 -> 1
+  setCount(prevCounter => prevCounter + 1); // 1 => 1 + 1 = 2
+  setCount(prevCounter => prevCounter + 1); // 2 => 2 + 1 = 3
+  // there is a callback in callback
+  // prev is the last updated state of count 
+
+  // setCount(count+1); 
+  // setCount(count+1); 
+  // setCount(count+1); 
 
   }
 
   function removeValue(){
-    setCount(prev => prev - 1);
+    setCount(prevCounter => prevCounter - 1); 
   }
 
   return (
     <div>
-      <h2>Counter value: {count}</h2>
+      <h2 maxLength="20">Counter value: {count}</h2>
       <button onClick={addValue}>Add</button>
       <br/>
       <br/>
