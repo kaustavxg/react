9e23a67b1e6a20d9d1485ddc9b2e54cd5b3e5324
kaustavgupta <kaustavgupta2311@gmail.com>
2025-09-21 14:34:18 +0530

    removed unnecessary code

--- 01basicreact/src/Chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h2>chai in react 01basicreact</h2>
-    )
-}
-
-export default Chai;
--- 01vitereact/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h3>chai is ready to code</h3>
-    )
-}
-
-export default Chai
