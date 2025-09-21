a48280cfb176a2000a50a299fffa5e396d53c69a
kaustavgupta <kaustavgupta2311@gmail.com>
2025-09-21 14:34:38 +0530

    tailwind and props

---
+++ tailwind-props-chai-aur-code/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <div>
+      <Card username="Kaustav" btnText="click me"/>
+      <Card username="Mohini" btnText="visit me" />
+    </div>
+     
+        
+  )
+}
+
+export default App
